f8e7a6e68681e6f4f6a355c8d9df29ad0c84da20
Rajib <prorajib6@gmail.com>
2025-05-10 20:04:14 +0600

    operator precedence

---
+++ operatorPrecedence.py
@@ -0,0 +1,12 @@
+# Guess the output of each answer before you click RUN
+# Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
+
+print((5 + 4) * 10 / 2)
+
+print(((5 + 4) * 10) / 2)
+
+print((5 + 4) * (10 / 2))
+
+print(5 + (4 * 10) / 2)
+
+print(5 + 4 * 10 // 2) # // this is is called floor division. it returns the result in integer but in flooring. lets say, 11//2 = 5.5 normally but it will show 5. it applys flooring to the result
