5723c89e457699f5e799169e482ef78fde95ad5b
Rajib chandra das <prorajib6@gmail.com>
2025-07-01 23:38:21 +0600

    float type casting

--- pythoncasting.py
+++ pythoncasting.py
@@ -1,6 +1,17 @@
+# integer type casting
 x = int(1)
 y = int(2.8)
 z = int("3")
 print(x)
 print(y)
 print(z)
+
+# float type casting
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
