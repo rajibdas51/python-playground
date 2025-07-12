5bcbf06f8df3af81427d58c22723a4e8de45a707
Rajib chandra Das <prorajib6@gmail.com>
2025-07-12 23:12:04 +0600

    list

---
+++ lists.py
@@ -0,0 +1,21 @@
+#Lists are used to store multiple items in a single variable.
+
+
+newlist = ['apple','banana','mango']
+print(newlist)
+
+list1 = ["abc", 34, True, 40, "male"]
+
+print(list1)
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+
+#It is also possible to use the list() constructor when creating a new list.
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
+thislist= list(('dog','cow','parrot'))
