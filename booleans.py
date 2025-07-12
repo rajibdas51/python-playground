9603450cc089161f7cd48198c340f9891dd4de04
Rajib chandra Das <prorajib6@gmail.com>
2025-07-12 23:07:30 +0600

    booleans

---
+++ booleans.py
@@ -0,0 +1,19 @@
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+# The bool() function allows you to evaluate any value, and give you True or False in return,
+
+print(bool("hello"))
+print(bool(15))
+print(bool(0))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
