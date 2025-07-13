8092d6f8fad61c0f92c1d72489c6fa27d683c8ab
Rajib chandra Das <prorajib6@gmail.com>
2025-07-13 23:59:40 +0600

    change list items

--- lists.py
+++ lists.py
@@ -53,3 +53,25 @@ thislist = ["apple", "banana", "cherry"]
 
 if "apple" in thislist :
     print("yes, apple is inthe fruits list!")
+
+
+
+    # Change list items
+
+    #change item value
+    thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+
+#Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon":
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+#Change the second value by replacing it with two new values:
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
