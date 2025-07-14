a5fd9b3e1d4cd01ab1651fd2c14022d137ff56f1
Rajib chandra Das <prorajib6@gmail.com>
2025-07-15 00:04:32 +0600

    extend list with tuples

--- lists.py
+++ lists.py
@@ -99,3 +99,12 @@ thislist = ["apple", "banana", "cherry"]
 tropical = ["mango", "pineapple", "papaya"]
 thislist.extend(tropical)
 print(thislist)
+
+
+#The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
+
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
