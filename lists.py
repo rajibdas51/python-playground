d1af7b2678909bdb53868877cc94f146c165cd56
Rajib chandra Das <prorajib6@gmail.com>
2025-07-15 22:44:06 +0600

    delete entire list

--- lists.py
+++ lists.py
@@ -126,3 +126,22 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 thislist.pop(1)
 print(thislist)
+
+#If you do not specify the index, the pop() method removes the last item.
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+#The del keyword also removes the specified index:
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+#The del keyword can also delete the list completely.
+
+
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
