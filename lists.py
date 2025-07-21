ad27a0e12fbd3959255833d294522b1547b861a1
Rajib chandra Das <prorajib6@gmail.com>
2025-07-21 23:56:56 +0600

    join two list

--- lists.py
+++ lists.py
@@ -218,3 +218,17 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 mylist = thislist.copy()
 print(mylist)
+
+#Make a copy of a list with the list() method:
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+#Join two list:
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
