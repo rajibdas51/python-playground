45f6081dd882dbd571261704db1bc563c8afa1e8
Rajib chandra Das <prorajib6@gmail.com>
2025-08-15 23:58:36 +0600

    delete tuple

--- tuples.py
+++ tuples.py
@@ -79,3 +79,9 @@ thistuple = ("apple", "banana", "cherry")
 y = list(thistuple)
 y.remove("apple")
 thistuple = tuple(y)
+
+#The del keyword can delete the tuple completely:
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
