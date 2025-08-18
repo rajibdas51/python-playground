ed911412fad4fa9c5edb79ce254c807790f0e958
Rajib chandra Das <prorajib6@gmail.com>
2025-08-18 23:40:08 +0600

    Join tuples

--- tuples.py
+++ tuples.py
@@ -138,3 +138,18 @@ i = 0
 while i < len(thistuple):
   print(thistuple[i])
   i = i + 1
+
+#Join two tuples:
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+#Multiply the fruits tuple by 2:
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
