9723d31a1ffe44fa4415e6272da3acf6d7a44d88
Rajib chandra Das <prorajib6@gmail.com>
2025-08-19 23:54:51 +0600

    add and access set items

--- set.py
+++ set.py
@@ -2,3 +2,50 @@
 
 thisset = {"apple", "banana", "cherry"}
 print(thisset)
+
+#Loop through the set, and print the values:
+
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+#Check if "banana" is present in the set:
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+
+#Check if "banana" is NOT present in the set:
+
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" not in thisset)
+
+#Add an item to a set, using the add() method:
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+
+#Add elements from tropical into thisset:
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+
+#Add elements of a list to at set:
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
