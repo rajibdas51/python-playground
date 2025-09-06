0243e8fa46a32c791e9d5e98fef98da29f5d2f7f
Rajib chandra Das <prorajib6@gmail.com>
2025-09-06 23:33:03 +0600

    python dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+#Create and print a dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+#Print the "brand" value of the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
