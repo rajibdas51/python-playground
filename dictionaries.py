887a89a9e5699fdb618ef344f8586b24a625138d
Rajib chandra Das <prorajib6@gmail.com>
2025-09-06 23:33:57 +0600

    access items

--- dictionaries.py
+++ dictionaries.py
@@ -15,3 +15,28 @@ thisdict = {
   "year": 1964
 }
 print(thisdict["brand"])
+
+
+#Get the value of the "model" key:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+
+print(x) #after the change
