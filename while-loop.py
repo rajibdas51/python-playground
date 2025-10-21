f079aa5fef72392c685f616ab0e85c0e0de0333a
Rajib chandra Das <prorajib6@gmail.com>
2025-10-21 23:41:54 +0600

    else statement

--- while-loop.py
+++ while-loop.py
@@ -22,3 +22,13 @@ while i < 6:
   if i == 3:
     continue
   print(i)
+
+
+#With the else statement we can run a block of code once when the condition no longer is true:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
