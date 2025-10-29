8587326e5e32cce8b628885fd71443ccc6bf2372
Rajib chandra Das <prorajib6@gmail.com>
2025-10-29 23:46:31 +0600

    functions in python

---
+++ functions.py
@@ -0,0 +1,21 @@
+#Python Functions
+#In Python, a function is defined using the def keyword, followed by a function name and parentheses:
+
+def greet():
+  print("Hello from a function")
+
+#To call a function, write its name followed by parentheses:
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+#You can call the same function multiple times:
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
