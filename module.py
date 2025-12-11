88972f88c03a64cad11fe76ddf06a34c2c30dc79
Rajib chandra Das <prorajib6@gmail.com>
2025-12-11 23:43:15 +0600

    re-naming a module

--- module.py
+++ module.py
@@ -28,3 +28,12 @@ import mymodule
 
 a = mymodule.person1["age"]
 print(a)
+
+
+#You can create an alias when you import a module, by using the as keyword:
+#Create an alias for mymodule called mx:
+
+import mymodule as mx
+
+a = mx.person1["age"]
+print(a)
