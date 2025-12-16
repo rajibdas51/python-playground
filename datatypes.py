0db5c4c713d36ff0885d1cb68fcf5a1b7e094d70
Rajib chandra Das <prorajib6@gmail.com>
2025-12-17 00:00:29 +0600

    numbers

---
+++ datatypes.py
@@ -0,0 +1,43 @@
+#Fundamental Data Types
+int
+float
+bool
+str
+tuple
+list
+set
+dict
+
+ # classes => custom types
+
+ # Specialized data types
+
+ #None
+
+# Numbers
+a= 394
+b =2.8
+j= 1j
+
+print(type(a))
+print(type(b))
+print(type(j))
+
+# floating point numbers
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Complex numbers are written with a "j" as the imaginary part:
+
+p = 3+5j
+q = 5j
+r = -5j
+print(type(p))
+print(type(q))
+print(type(r))
+
+
