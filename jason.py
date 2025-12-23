6a6b9a99047f883d6e8922e10514cce16bedb294
Rajib chandra Das <prorajib6@gmail.com>
2025-12-24 00:00:16 +0600

    converte from python to json

--- jason.py
+++ jason.py
@@ -10,3 +10,21 @@ y = json.loads(x)
 
 # the result is a Python dictionary:
 print(y["age"])
+
+
+#Convert from Python to JSON:
+
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
