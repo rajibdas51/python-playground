3644c03f2f1190c790a81992b1b37b51a5541a66
Rajib chandra Das <prorajib6@gmail.com>
2025-12-24 23:52:22 +0600

    findall() funciton

--- regex.py
+++ regex.py
@@ -4,3 +4,11 @@ import re
 
 txt = "The rain in Spain"
 x = re.search("^The.*Spain$", txt)
+
+#The findall() function returns a list containing all matches.
+
+import re
+
+txt = "The rain in Spain"
+x = re.findall("ai", txt)
+print(x)
