5bdd32c9e99838c000dfa18260a6d1c304d2fb75
Rajib chandra Das <prorajib6@gmail.com>
2025-12-25 23:58:26 +0600

    search function

--- regex.py
+++ regex.py
@@ -12,3 +12,12 @@ import re
 txt = "The rain in Spain"
 x = re.findall("ai", txt)
 print(x)
+
+#Search for the first white-space character in the string:
+
+import re
+
+txt = "The rain in Spain"
+x = re.search("\s", txt)
+
+print("The first white-space character is located in position:", x.start())
