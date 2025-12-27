d18666b7e257764652b133837f33b275791a8e45
Rajib chandra Das <prorajib6@gmail.com>
2025-12-28 00:00:22 +0600

    finally block

--- tryexcept.py
+++ tryexcept.py
@@ -14,3 +14,13 @@ except:
   print("Something went wrong")
 else:
   print("Nothing went wrong")
+  
+#The finally block, if specified, will be executed regardless if the try block raises an error or not.
+
+
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
