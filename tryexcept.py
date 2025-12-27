c2d169304fb5f007b1af731e658886953f1afa35
Rajib chandra Das <prorajib6@gmail.com>
2025-12-27 23:59:53 +0600

    else keyword

--- tryexcept.py
+++ tryexcept.py
@@ -4,3 +4,13 @@ try:
   print(x)
 except:
   print("An exception occurred")
+
+
+#  In this example, the try block does not generate any error:
+
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
