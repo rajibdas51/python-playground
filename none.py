dd1fd683b0774256f7b4d64f6c4bc5e9a975a3f0
Rajib chandra Das <prorajib6@gmail.com>
2026-01-03 23:57:06 +0600

    python none

---
+++ none.py
@@ -0,0 +1,7 @@
+#None is a special constant in Python that represents the absence of a value.
+#Variables can be assigned None to indicate "no value" or "not set".
+
+x = None
+print(x)
+x = None
+print(type(x))
